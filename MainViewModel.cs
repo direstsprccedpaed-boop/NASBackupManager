815eb6465371bd7cc6a4bbbaaf3af65f19d976bf
direstsprccedpaed-boop <direst.spr.cced.paed@gmail.com>
2026-08-05 15:40:56 +0200

    Update MainViewModel.cs

--- MainViewModel.cs
+++ MainViewModel.cs
@@ -1,3 +1,6 @@
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
